184261b41521a709ad0fc9111c8e58629a820065
SquatCod <77495531+SquatCod@users.noreply.github.com>
2022-08-14 17:07:17 -0230

    commiting interface, and changes in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main implements ASCII_hangman {
+
+
+    public static void main (String[] args) {
+
+        System.out.println("Enter a character to save the hangman!\n" + parts_0);
+        String input = new Scanner (System.in).next();
+
     }
 }
